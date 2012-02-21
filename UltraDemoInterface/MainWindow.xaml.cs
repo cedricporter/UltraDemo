0627216a53f9b9fc2d6d9c2ef097ff573f582152
kid <963262214@qq.com>
2012-02-22 01:41:31 +0800

    这他妈的终于解决了Resize时界面布局问题了
    Signed-off-by: kid <963262214@qq.com>

--- UltraDemoInterface/MainWindow.xaml.cs
+++ UltraDemoInterface/MainWindow.xaml.cs
@@ -24,40 +24,6 @@ namespace UltraDemoInterface
             InitializeComponent();
         }
 
-        /*/// <summary>
-        /// 显示编辑面板，隐藏演示面板。
-        /// 作者：屠文翔
-        /// </summary>
-        private void EditorViewButton_Checked(object sender, RoutedEventArgs e)
-        {
-            if (this.IsInitialized)
-            {
-                EditorContainer.Visibility = Visibility.Visible;
-                DemoContainer.Visibility = Visibility.Hidden;
-            }
-        }
-
-        /// <summary>
-        /// 显示演示面板，隐藏编辑面板。
-        /// 作者：屠文翔
-        /// </summary>
-        private void DemoViewButton_Checked(object sender, RoutedEventArgs e)
-        {
-            if (this.IsInitialized)
-            {
-                EditorContainer.Visibility = Visibility.Hidden;
-                DemoContainer.Visibility = Visibility.Visible;
-            }
-        }
-
-        /// <summary>
-        /// 切换至演示面板动画。
-        /// 作者：屠文翔
-        /// </summary>
-        private void DemoViewButton_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }*/
     }
     
 }
