00169844a425e5e5648a76bc619646865a7dcc4a
Cedric Porter <cedricporter@gmail.com>
2012-04-20 13:14:41 +0800

    数组动画

--- NewArrayAnimationPlugin/ArrayAnimation.cs
+++ NewArrayAnimationPlugin/ArrayAnimation.cs
@@ -22,6 +22,8 @@ namespace UltraDemoInterface
             watchedList = new List<String>();
             watchedList.Add("watchedArray");
             watchedList.Add("watchedSize");
+            watchedList.Add("cnt");
+            watchedList.Add("b");
             //System.Windows.MessageBox.Show("ArrayAnimation Created");
 
             //testing
@@ -35,44 +37,54 @@ namespace UltraDemoInterface
             
         }
 
-        public void DrawArray(int* array, int size)
-        {
-            animationContainer.Children.Clear();
+        public void DrawArray(int* array, int size, int y, string name)
+        { 
+            TranslateTransform infots = new TranslateTransform((-260) % 300, y);
+            TextBlock infoText = new TextBlock();
+            infoText.RenderTransform = infots;
+            infoText.Text = name;
+            infoText.Height = 30;
+            infoText.Width = 50;
+            animationContainer.Children.Add(infoText); 
 
-            int j = 0;
             for (int i = 0; i < size; i++)
             {
                 Rectangle rect = new Rectangle();
 
-                TextBox text = new TextBox();
+                TextBlock text = new TextBlock();
                 text.Text = array[i].ToString();
                 text.Height = 30;
+                text.Width = 50;
 
-                rect.Height = array[i] /  50;
+                rect.Height = array[i] ;
                 rect.Width = 30;
-                rect.Fill = new SolidColorBrush(Colors.Red);
+                rect.Fill = new SolidColorBrush(Colors.LightBlue);
 
-                TranslateTransform ts = new TranslateTransform((-200 + 50 * i) % 300, j * 30);
 
-                rect.RenderTransform = ts;
-                text.RenderTransform = ts;
+                TranslateTransform textts = new TranslateTransform((-200 + 50 * i) % 300, y);
+                TranslateTransform backts = new TranslateTransform((-215 + 50 * i) % 300, y);
 
-                //animationContainer.Children.Add(rect); 
+                text.RenderTransform = textts;
+                rect.RenderTransform = backts;
+
+                animationContainer.Children.Add(rect); 
                 animationContainer.Children.Add(text);
-
-                if ((-200 + 50 * i) % 300 == 0)
-                    j++;
             } 
         }
 
         public override void BeginRender(Object sender, EventArgs e, Dictionary<String, UInt32> map)
         {
             int* array = (int*)*(int*)map["watchedArray"];
+            int* cnt = (int*)*(int*)map["cnt"];
+            int* b = (int*)*(int*)map["b"];
             int size = *(int*)map["watchedSize"];
 
             if (array != (int*)0)
             {
-                DrawArray(array, size);
+                animationContainer.Children.Clear();
+                DrawArray(array, size, 100, "array");
+                DrawArray(cnt, size, 0, "cnt");
+                DrawArray(b, size, -100, "b");
             }
 
         }
--- UltraDemoInterface/code/基数排序.hpp
+++ UltraDemoInterface/code/基数排序.hpp
@@ -1,89 +1,89 @@
 int RadixSort( int * watchedArray, int * b, int * cnt,int watchedSize, int k, int r)
 {
-	int i,j,temp,temp1,temp2,temp3;
-	int rtok;
-	i = 0;
-	rtok = 1;
-	while ( i < k)
-	{
-		j = 0;
-		while ( j < r) 
-		{
-			cnt[j] = 0;
-			j = j + 1;
-		}
-		j = 0;
-		while ( j < watchedSize)
-		{
-                                       temp1 = watchedArray[j]/rtok;
-			temp2 = temp1/r;
-			temp = temp1 - temp2*r;
-			cnt[temp] = cnt[temp] + 1;
-			j = j + 1;
-		}
+    int i,j,temp,temp1,temp2,temp3;
+    int rtok;
+    i = 0;
+    rtok = 1;
+    while ( i < k)
+    {
+        j = 0;
+        while ( j < r) 
+        {
+            cnt[j] = 0;
+            j = j + 1;
+        }
+        j = 0;
+        while ( j < watchedSize)
+        {
+            temp1 = watchedArray[j]/rtok;
+            temp2 = temp1/r;
+            temp = temp1 - temp2*r;
+            cnt[temp] = cnt[temp] + 1;
+            j = j + 1;
+        }
 
-		j = 1;
-		while ( j < r)
-		{
-                                                     temp = j - 1;
-			cnt[j] = cnt [temp] + cnt[j];
-			j = j + 1;
-		}
-		j = watchedSize - 1;
-		while ( j >= 0)
-		{
-			temp1 = watchedArray[j]/rtok;
-			temp2 = temp1/r;
-			temp3 = temp1 - temp2*r;
-			cnt[temp3] = cnt[temp3] - 1;
-                                                    temp = cnt[temp3];
-			b[temp] = watchedArray[j];
-			j = j - 1;
-		}
-		j = 0;
-		while ( j < watchedSize )
-		{
-			watchedArray[j] = b[j];
-			j = j + 1;
-		}
-		i = i + 1;
-		rtok = rtok * r;
-	}
-          return 0;
+        j = 1;
+        while ( j < r)
+        {
+            temp = j - 1;
+            cnt[j] = cnt [temp] + cnt[j];
+            j = j + 1;
+        }
+        j = watchedSize - 1;
+        while ( j >= 0)
+        {
+            temp1 = watchedArray[j]/rtok;
+            temp2 = temp1/r;
+            temp3 = temp1 - temp2*r;
+            cnt[temp3] = cnt[temp3] - 1;
+            temp = cnt[temp3];
+            b[temp] = watchedArray[j];
+            j = j - 1;
+        }
+        j = 0;
+        while ( j < watchedSize )
+        {
+            watchedArray[j] = b[j];
+            j = j + 1;
+        }
+        i = i + 1;
+        rtok = rtok * r;
+    }
+    return 0;
 }
 int main ()
 {
-                int a[ 10 ],b[10],cnt[10];
-	int i;
-	i = 0;
-	while ( i < 10 )
-	{
-		a[ i ] = 50 - i * 2;
-		i = i + 1;
-	}
-	a[ 5 ] = 542;
-	a[ 2 ] = 1234;
-	a[ 8 ] = 43;
+    int a[ 10 ],b[10],cnt[10];
+    int i;
+    i = 0;
+    while ( i < 10 )
+    {
+        a[ i ] = 50 - i * 2;
+        i = i + 1;
+    }
+    a[ 5 ] = 542;
+    a[ 2 ] = 1234;
+    a[ 8 ] = 43;
 
-	i = 0;
-	while ( i < 10 )
-	{
-		write a[ i ];
-		write 1;
-		i = i + 1;
-	}
-	write 0;
+    i = 0;
+    while ( i < 10 )
+    {
+        write a[ i ];
+        write 1;
+        i = i + 1;
+    }
+    write 0;
 
-	RadixSort(&a[0],&b[0],&cnt[0],10,4,10);
-	i = 0;
-	while ( i < 10 )
-	{
-		write a[ i ];
-		write 1;
-		i = i + 1;
-	}
+    RadixSort(&a[0],&b[0],&cnt[0],10,4,10);
+    i = 0;
+    while ( i < 10 )
+    {
+        write a[ i ];
+        write 1;
+        i = i + 1;
+    }
 
-	return 0;
+    return 0;
 }
 
 
